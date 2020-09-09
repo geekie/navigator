9d29c55da0d0b98f6867a343d3d1be621ff61860
Rodrigo Almeida <sherman@geekie.com.br>
2020-09-09 18:28:39 -0300

    Adding Navigator.waitForPendingNavigations() to enable some level of chaining
    Summary:
    
    At Geekie, we have one particular use case in which one screen may "redirect" to another screen (via navigator.replace). Sometimes, this redirect happens as soon as the first screen mounts, and in this case there is a race condition: the call to replace() may happen before the lock is released, causing the redirect to be silently ignored.
    
    This commit exposes a waitForPendingNavigations() API that allows navigator clients to await for the current transition to finish prior to issuing new navigation commands. This is insufficient to implement a true queueing mechanism, but then there seems to be no currently use cases that would require a true queueing system.
    
    Test Plan:
    
    - Used over Geekie to fix a bug in the HighlightOpener screen
    - Ran `npx jest`
    - Ran `npx eslint` over all *.js files

--- src/utils.js
+++ src/utils.js
@@ -6,13 +6,3 @@ let _uid = 0;
 export function uid() {
   return _uid++;
 }
-
-let _lock = false;
-export const lock = {
-  acquire() {
-    return _lock ? false : (_lock = true);
-  },
-  release() {
-    _lock = false;
-  }
-};
